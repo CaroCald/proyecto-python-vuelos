88ae21bc587b942f76ad7f6c6766a08ac25d94fd
Carolina Calderon <diana.calderon@epn.edu.ec>
2018-10-24 17:21:53 -0500

    implementacion de registro y consulta

--- registro_vuelos.py
+++ registro_vuelos.py
@@ -1,29 +1,14 @@
-
-path = './registro_vuelos.txt'
-
-
 def registro_vuelos(opcion):
+    pathArchivo = './registro_vuelos.txt'
+
     def ingresar_vuelo(hora, destino):
-        return agregar_a_archivo(path, hora, destino)
+        return agregar_a_archivo(pathArchivo, f"Hora: {hora}", f"Destino: {destino} \n")
 
     def actualizar_vuelo():
-        return
+        return print('actualizar')
 
     def eliminar_vuelo():
-        return
-
-    def consultar_vuelo(path):
-        return leer_archivo(path)
-
-    def leer_archivo(path):
-        try:
-            archivo_abierto = open(path)  # por defecto es la r -> read
-            arreglo_lineas = archivo_abierto.readlines()
-            for linea in arreglo_lineas:
-                print(linea)
-            archivo_abierto.close()
-        except Exception:
-            print("No se pudo leer")
+        return print('eliminar vuelo')
 
     def agregar_a_archivo(path, *lineas_a_escribir):
         try:
@@ -32,22 +17,37 @@ def registro_vuelos(opcion):
                 archivo_abierto.write("\n" + linea)
 
             archivo_abierto.close()
+            return print('Ingresado con exito!')
         except Exception:
-            print("No se pudo leer")
+            return print("No se pudo leer")
 
-    def elegir_opcion():
-        return {
-            'ingresar': ingresar_vuelo(),
-            'actualizar': actualizar_vuelo(),
-            'eliminar': eliminar_vuelo(),
-            'consultar': consultar_vuelo()
-        }[opcion]
-    return elegir_opcion()
+    def leer_archivo(path=pathArchivo):
+        try:
+            archivo_abierto = open(path)  # por defecto es la r -> read
+            arreglo_lineas = archivo_abierto.readlines()
+            for linea in arreglo_lineas:
+                print(linea)
+            archivo_abierto.close()
+        except Exception:
+            return print("No se pudo leer")
+
+    def consultar_vuelo(path=pathArchivo):
+        return leer_archivo(path)
+
+    def seleccion():
+        if opcion == "1":
+            hora = input("ingrese hora ")
+            destino=input("destino")
+            ingresar_vuelo(hora=hora, destino=destino)
+        elif opcion == "2":
+            consultar_vuelo(pathArchivo)
+    return seleccion()
 
 
-opcion = input("Ingrese una opcion \n 1. Ingresar: \n"
-               "")
-registro_vuelos(opcion)
+eleccion = input("Ingrese una opcion \n "
+               "1. Ingresar: \n"
+               "2. Actualizar \n"
+               "3. Consultar \n"
+               "4. Eliminar")
 
-hora = input("hora")
-destino
+registro_vuelos(eleccion)
